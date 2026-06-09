267efd79e7f637a9798d2159c18c56e8d121bdff
EgorPigalev <1pigaleve@mail.ru>
2026-06-09 19:19:57 +0300

    Добавьте файлы проекта.

---
+++ Program_password_verification/FileName.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <cctype>
+using namespace std;
+
+bool validatePassword(const string* password) {
+    if (password == nullptr) {
+        return false;
+    }
+    if (password->length() < 8) {
+        return false;
+    }
+    bool hasDigit = false;
+    bool hasUpper = false;
+    for (size_t i = 0; i < password->length(); ++i) {
+        char ch = (*password)[i];
+        if (ch == ' ') {
+            return false;
+        }
+        if (isdigit(ch)) {
+            hasDigit = true;
+        }
+        if (isupper(ch)) {
+            hasUpper = true;
+        }
+    }
+    return hasDigit && hasUpper;
+}
